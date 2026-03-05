aaaa58a9257b389b9de5bfc894da2bf61c39b930
anshikagupta100 <anshika150720055@gmail.com>
2026-03-05 19:54:44 +0530

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { FrameComponent } from './layout/frame/frame';
+import { Dashboard} from './pages/dashboard/dashboard';
+import { Analytics} from './pages/analytics/analytics';
+import { Products} from './pages/products/products';
+import { Inventory} from './pages/inventory/inventory';
+import { Orders} from './pages/orders/orders';
+import { Customers} from './pages/customers/customers';
+import { Coupons} from './pages/coupons/coupons';
+import { Settings} from './pages/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: FrameComponent,
+    children: [
+      { path: 'dashboard', component: Dashboard},
+      { path: 'analytics', component: Analytics},
+      { path: 'products', component: Products},
+      { path: 'inventory', component: Inventory},
+      { path: 'orders', component: Orders},
+      { path: 'customers', component: Customers},
+      { path: 'coupons', component: Coupons},
+      { path: 'settings', component: Settings},
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+  }
+];
